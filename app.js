fe7103ebf5c8d8e5d33de1dc3f71800ee1e30496
Pragya Rai <pragyar@thoughtworks.com>
2019-02-09 12:40:25 +0530

    Added request module for request forwarding

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const request = require('request');
 const app = express();
 const PORT = 3000;
 
+
 app.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+app.get('/todo', (req, res) => {
+    request.get('http://todo:8080/todo', (error, response, body) => {
+        if(err){
+            console.log(err);
+            res.status(500);
+            res.end();
+        }
+        res.status(200);
+        res.send(body);
+    });
 })
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
